3e77eaf113727ca9ffbb6a4f3d38b1ca13a15163
NirajFegade98 <91453457+NirajFegade98@users.noreply.github.com>
2022-02-19 17:53:25 +0530

    Add files via upload

---
+++ DotNetExam/MVC APP Product/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DotNetLabExam.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Productid { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage ="Please enter Product Name")]
+        [Display(Name ="ProductName")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Rate")]
+        [Display(Name = "Rate")]
+        public decimal Rate { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please enter Description")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please enter Category Name")]
+        [Display(Name = "CategoryName")]
+        public string CategoryName { get; set; }
+    }
+}
---
+++ DotNetExam/MVC APP Product/ProductsController.cs
@@ -0,0 +1,127 @@
+﻿using DotNetLabExam.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetLabExam.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Products> lst = new List<Products>();
+            SqlConnection sc = new SqlConnection();
+            sc.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DotNetExam;Integrated Security=True";
+            sc.Open();
+            SqlCommand scm = new SqlCommand();
+            scm.Connection = sc;
+            scm.CommandType = System.Data.CommandType.StoredProcedure;
+            scm.CommandText = "displayProducts";
+            SqlDataReader dr = scm.ExecuteReader();
+            while (dr.Read())
+            {
+                lst.Add(new Products { Productid = (int)dr["Productid"], ProductName = (string)dr["ProductName"], Rate = (decimal)dr["Rate"], Description = (string)dr["Description"], CategoryName = (string)dr["CategoryName"] });
+            }
+            dr.Close();
+            sc.Close();
+            return View(lst);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Products obj = new Products();
+            SqlConnection sc = new SqlConnection();
+            sc.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DotNetExam;Integrated Security=True";
+            sc.Open();
+            SqlCommand scm = new SqlCommand();
+            scm.Connection = sc;
+            scm.CommandType = System.Data.CommandType.StoredProcedure;
+            scm.CommandText = "updatedetails";
+            scm.Parameters.AddWithValue("@id",id);
+            SqlDataReader dr = scm.ExecuteReader();
+            while (dr.Read())
+            {
+               obj = new Products { Productid = (int)dr["Productid"], ProductName = (string)dr["ProductName"], Rate = (decimal)dr["Rate"], Description = (string)dr["Description"], CategoryName = (string)dr["CategoryName"] };
+            }
+            dr.Close();
+            sc.Close();
+            return View(obj);           
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Products obj)
+        {
+            SqlConnection sc = new SqlConnection();
+            sc.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DotNetExam;Integrated Security=True";
+            sc.Open();
+            SqlCommand scm = new SqlCommand();
+            scm.Connection = sc;
+            scm.CommandType = System.Data.CommandType.StoredProcedure;
+            scm.CommandText = "updateProducts";
+            scm.Parameters.AddWithValue("@id", id);
+            scm.Parameters.AddWithValue("@ProductName", obj.ProductName);
+            scm.Parameters.AddWithValue("@Rate", obj.Rate);
+            scm.Parameters.AddWithValue("@Description", obj.Description);
+            scm.Parameters.AddWithValue("@CategoryName", obj.CategoryName);
+            scm.ExecuteNonQuery();
+            sc.Close();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
